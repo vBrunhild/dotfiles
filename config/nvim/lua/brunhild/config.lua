65c473e70a7bf5045566f45eadc4f2b290db7cc6
vBrunhild <brunomoretti100@gmail.com>
2025-07-14 01:33:09 +0000

    added clipboard provider for neovim

---
+++ config/nvim/lua/brunhild/config.lua
@@ -0,0 +1,6 @@
+vim.wo.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
--- user/wrapped/neovim.nix
+++ user/wrapped/neovim.nix
@@ -7,30 +7,31 @@ let
     withRuby = false;
     withNodeJs = false;
     customRC = ''
-	:luafile ~/.config/nvim/init.lua
+      :luafile ~/.config/nvim/init.lua
     '';
 
     plugins = (attrValues {
       inherit (pkgs.vimPlugins)
         onedark-nvim
-	telescope-nvim
-	undotree
-	gitsigns-nvim
-	lualine-nvim
-	indent-blankline-nvim-lua
-	nvim-autopairs
-	comment-nvim
-	nvim-colorizer-lua
-	lsp_lines-nvim
-	trouble-nvim
+        telescope-nvim
+        undotree
+        gitsigns-nvim
+        lualine-nvim
+        nvim-osc52
+        indent-blankline-nvim-lua
+        nvim-autopairs
+        comment-nvim
+        nvim-colorizer-lua
+        lsp_lines-nvim
+        trouble-nvim
         harpoon
-	nvim-lspconfig
-	nvim-cmp
-	luasnip
-	friendly-snippets
-	cmp-nvim-lsp
-	cmp-buffer
-	cmp-path
+        nvim-lspconfig
+        nvim-cmp
+        luasnip
+        friendly-snippets
+        cmp-nvim-lsp
+        cmp-buffer
+        cmp-path
       ;
     }) ++ [ pkgs.vimPlugins.nvim-treesitter.withAllGrammars ];
   };
@@ -39,3 +40,4 @@ in
     name = "neovim-wrapped";
     paths = [ (pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped neovimConfig) ];
   }
+
