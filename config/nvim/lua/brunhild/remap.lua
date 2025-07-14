b8e96cd378c71ff294c225b47de4798f3345c148
vBrunhild <brunomoretti100@gmail.com>
2025-07-14 21:02:48 +0000

    Add keymaps for diagnostics

--- config/nvim/lua/brunhild/remap.lua
+++ config/nvim/lua/brunhild/remap.lua
@@ -1,4 +1,6 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader> ", vim.cmd.Ex)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic' })
 
