20b0c13b0a698c661b1ab12f4d1e68e9d166777e
vBrunhild <brunomoretti100@gmail.com>
2025-07-15 21:04:11 +0000

    Added some custom configurations on neovim

--- config/nvim/lua/brunhild/config.lua
+++ config/nvim/lua/brunhild/config.lua
@@ -1,7 +1,16 @@
 vim.wo.relativenumber = true
+vim.wo.signcolumn = "yes"
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.colorcolumn = "121"
+vim.opt.updatetime = 100
+
+vim.o.completeopt = "menuone,noinsert,noselect"
+
+-- fold stuff
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.lsp.foldexpr()"
+vim.o.foldlevel = 99
 
