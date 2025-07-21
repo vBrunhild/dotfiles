e53b97913cf6041e24320a63621af1cc5cb50768
vBrunhild <brunomoretti100@gmail.com>
2025-07-21 20:48:48 +0000

    Created rust development flake

---
+++ devflakes/rust/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "My dev flake for Rust!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+  let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "aarch64-linux"
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in {
+    devShells = forAllSystems(system:
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.bacon
+          ];
+
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      }
+    );
+  };
+}
