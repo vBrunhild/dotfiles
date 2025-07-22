0c61d34d805a27d5b57329a41f115abaa18f0739
vBrunhild <brunomoretti100@gmail.com>
2025-07-22 02:49:23 +0000

    Move all remaps to `remap.lua`

--- config/nvim/lua/brunhild/remap.lua
+++ config/nvim/lua/brunhild/remap.lua
@@ -1,8 +1,18 @@
 local wk = require("which-key")
+local telescope = require("telescope.builtin")
 
 vim.g.mapleader = " "
 
 wk.add({
-    { "<leader> ", vim.cmd.Ex, desc = "Opens neovim explorer." }
+    { "<leader> ", vim.cmd.Ex, desc = "Opens neovim explorer" },
+
+    -- telescope
+    { "<leader>ff", telescope.find_files, desc = "Telescope find files" },
+    { "<leader>fg", telescope.git_files, desc = "Telescope find git files" },
+
+    -- lsp
+    { "<leader>ld", vim.lsp.buf.signature_help, desc = "Signature documentation" },
+    { "<leader>lr", vim.lsp.buf.rename, desc = "Rename all references" },
+    { "<leader>lv", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>", desc = "Goto definition in vertical split" }
 })
 
