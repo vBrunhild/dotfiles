74f62e07d47f4f3583861c904eb6f413a61bf881
vBrunhild <brunomoretti100@gmail.com>
2025-07-22 21:09:56 +0000

    Create go dev flake

---
+++ devflakes/go/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "My dev flake for Go!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+  let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "aarch64-linux"
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in {
+    devShells = forAllSystems(system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.delve
+            pkgs.air
+          ];
+        };
+      }
+    );
+  };
+}
