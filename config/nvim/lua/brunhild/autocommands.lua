d6abdf7e513c43bb666f8397a928066b3f325ba2
vBrunhild <brunomoretti100@gmail.com>
2025-07-23 18:46:04 +0000

    configs for mini.files and mini.clue

--- config/nvim/lua/brunhild/autocommands.lua
+++ config/nvim/lua/brunhild/autocommands.lua
@@ -1,8 +1,24 @@
+local minifiles = require("mini.files")
+
 vim.api.nvim_create_autocmd("VimEnter", {
     callback = function()
         if vim.fn.argv(0) == "" then
             require("telescope.builtin").find_files()
         end
+    end
+})
+
+local map_to_buffer = function(buffer, lhs, rhs, desc)
+    vim.keymap.set("n", lhs, rhs, { buffer = buffer, desc = desc })
+end
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "MiniFilesBufferCreate",
+    callback = function(args)
+        local buffer = args.data.buf_id
+
+        map_to_buffer(buffer, "<A-Right>", function() minifiles.go_in({ close_on_file = true }) end, "Go in file")
+        map_to_buffer(buffer, "<A-Left>", minifiles.go_out, "Go out of file")
     end
 })
 
---
+++ config/nvim/lua/brunhild/plugins/minifiles.lua
@@ -0,0 +1,10 @@
+ require("mini.files").setup({
+    windows = {
+        max_number = 3,
+        preview = true,
+        width_focus = 25,
+        width_nofocus = 25,
+        width_preview = 25
+    }
+ })
+
