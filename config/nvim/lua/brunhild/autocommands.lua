10b6e82b9b76fc5efac485d9b74ea2db4b34e703
vBrunhild <brunomoretti100@gmail.com>
2025-07-23 21:38:49 +0000

    setup zellij nav, still needs the plugin though

--- config/nvim/lua/brunhild/autocommands.lua
+++ config/nvim/lua/brunhild/autocommands.lua
@@ -1,3 +1,4 @@
+local telescope = require("telescope.builtin")
 local minifiles = require("mini.files")
 local minitrailspace = require("mini.trailspace")
 
@@ -5,14 +6,17 @@ local map_to_buffer = function(buffer, lhs, rhs, desc)
     vim.keymap.set("n", lhs, rhs, { buffer = buffer, desc = desc })
 end
 
+-- Supposed to open vim with Telescope but somehow doesn't work?
 vim.api.nvim_create_autocmd("VimEnter", {
     callback = function()
         if vim.fn.argv(0) == "" then
-            require("telescope.builtin").find_files()
+            -- minifiles.close() DOESN'T WORK!
+            telescope.find_files()
         end
     end
 })
 
+-- This lets me navigate minifiles hjkl is stupid
 vim.api.nvim_create_autocmd("User", {
     pattern = "MiniFilesBufferCreate",
     callback = function(args)
@@ -23,9 +27,14 @@ vim.api.nvim_create_autocmd("User", {
     end
 })
 
+-- Auto trim last lines on bw
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
     callback = function()
         minitrailspace.trim_last_lines()
     end
+})
+
+-- Zellij compat
+vim.api.nvim_create_autocmd("VimLeave", {
+    command = "silent !zellij action switch-mode normal"
 })
