f3082b26cdab0833e4c0fedce62cc90fde33f9e0
vBrunhild <brunomoretti100@gmail.com>
2025-07-25 17:58:26 +0000

    change `<cmd>` to `:` for consistency

--- config/nvim/lua/brunhild/keymaps.lua
+++ config/nvim/lua/brunhild/keymaps.lua
@@ -17,26 +17,28 @@ map("n", "<Left>", function() origami.h() end)
 map("n", "<Right>", function() origami.l() end)
 
 -- find
-map("n", "<leader>ff", "<cmd>lua MiniPick.builtin.files({ tool = 'rg' })<cr>", { desc = "Find files" })
-map("n", "<leader>fg", "<cmd>lua MiniPick.builtin.grep_live({ tool = 'rg' })<cr>", { desc = "Find grep" })
-map("n", "<leader>fb", "<cmd>lua MiniPick.builtin.buffers()<cr>", { desc = "Find buffers" })
-map("n", "<leader>fh", "<cmd>lua MiniExtra.pickers.git_hunks()<cr>", { desc = "Find hunks" })
-map("n", "<leader>fd", "<cmd>lua MiniExtra.pickers.diagnostic()", { desc = "Find diagnostics" })
+map("n", "<leader>ff", ":lua MiniPick.builtin.files({ tool = 'rg' })<cr>", { desc = "Find files" })
+map("n", "<leader>fg", ":lua MiniPick.builtin.grep_live({ tool = 'rg' })<cr>", { desc = "Find grep" })
+map("n", "<leader>fb", ":lua MiniPick.builtin.buffers()<cr>", { desc = "Find buffers" })
+map("n", "<leader>fh", ":lua MiniExtra.pickers.git_hunks()<cr>", { desc = "Find hunks" })
+map("n", "<leader>fd", ":lua MiniExtra.pickers.diagnostic()", { desc = "Find diagnostics" })
 
 -- lsp
 map("n", "<leader>ld", vim.lsp.buf.definition, { desc = "LSP goto definition" })
-map("n", "<Leader>lv", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", { desc = "LSP goto definition in vertical split" })
+map("n", "<Leader>lv", ":vsplit | lua vim.lsp.buf.definition()<CR>", { desc = "LSP goto definition in vertical split" })
 map("n", "<Leader>lr", vim.lsp.buf.rename, { desc = "LSP rename" })
 map("n", "<leader>lh", vim.lsp.buf.hover, { desc = "LSP hover" })
 map("n", "<leader>la", vim.lsp.buf.code_action, { desc = "LSP code action" })
 
 -- navigation / hjkl wtf?
-map("n", "<A-Left>", "<cmd>ZellijNavigateLeftTab<cr>", { desc = "Navigate left" })
-map("n", "<A-Right>", "<cmd>ZellijNavigateRightTab<cr>", { desc = "Navigate right" })
-map("n", "<A-Up>", "<cmd>ZellijNavigateUp<cr>", { desc = "Navigate up" })
-map("n", "<A-Down>", "<cmd>ZellijNavigateDown<cr>", { desc = "Navigate down" })
+map("n", "<A-Left>", ":ZellijNavigateLeftTab<cr>", { desc = "Navigate left" })
+map("n", "<A-Right>", ":ZellijNavigateRightTab<cr>", { desc = "Navigate right" })
+map("n", "<A-Up>", ":ZellijNavigateUp<cr>", { desc = "Navigate up" })
+map("n", "<A-Down>", ":ZellijNavigateDown<cr>", { desc = "Navigate down" })
 map("n", "<C-Left>", ":bnext<CR>", { desc = "Next buffer" })
 map("n", "<C-Right>", ":bprevious<CR>", { desc = "Previous buffer" })
+map("n", "<C-Left>", ":silent bnext<CR>", { desc = "Next buffer" })
+map("n", "<C-Right>", ":silent bprevious<CR>", { desc = "Previous buffer" })
 
 -- substitute
 map("n", "s", substitute.operator, { desc = "Substitution operator" })
