a812e4aa6fd4d41c598619f49468817a9a310618
vBrunhild <brunomoretti100@gmail.com>
2025-07-28 03:48:38 +0000

    correct typo

--- config/nvim/lua/brunhild/plugins/conform.lua
+++ config/nvim/lua/brunhild/plugins/conform.lua
@@ -15,9 +15,9 @@ require("conform").setup({
 
 vim.api.nvim_create_user_command("Format", function(args)
     local range = nil
-    
+
     if args.count ~= -1 then
-        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.lin2, true)[1]
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
         range = {
             start = { args.line1, 0 },
             ["end"] = { args.line2, end_line:len() }
@@ -26,4 +26,3 @@ vim.api.nvim_create_user_command("Format", function(args)
 
     require("conform").format({ async = true, lsp_format = "fallback", range = range })
 end, { range = true })
-
