50bc53b243a536a5e0806766f8f36002a54ee032
vBrunhild <brunomoretti100@gmail.com>
2025-07-28 20:54:56 +0000

    add bottom package

--- user/packages.nix
+++ user/packages.nix
@@ -1,26 +1,30 @@
 { pkgs, inputs, ... }:
 let
   inherit (builtins) attrValues;
-in {
-  environment.systemPackages = attrValues {
-    inherit (pkgs)
-      nh
-      stow
-      gcc
-      clang
-      git
-      git-credential-manager
-      curl
-      direnv
-      fish
-      docker
-      gitui
-      starship
-      zoxide
-      uutils-coreutils-noprefix
-      ripgrep
-      wezterm
-      zellij
-    ;
-  } ++ [inputs.zen-browser.packages.${pkgs.system}.default];
+in
+{
+  environment.systemPackages =
+    attrValues {
+      inherit (pkgs)
+        nh
+        stow
+        gcc
+        clang
+        git
+        git-credential-manager
+        curl
+        direnv
+        fish
+        docker
+        gitui
+        starship
+        zoxide
+        uutils-coreutils-noprefix
+        ripgrep
+        wezterm
+        zellij
+        bottom
+        ;
+    }
+    ++ [ inputs.zen-browser.packages.${pkgs.system}.default ];
 }
