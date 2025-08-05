85a42ae0c038ce35fc16a667d83a4127a843c54e
vBrunhild <brunomoretti100@gmail.com>
2025-08-05 02:33:44 +0000

    add cursor line

--- config/nvim/lua/brunhild/config.lua
+++ config/nvim/lua/brunhild/config.lua
@@ -1,35 +1,34 @@
 vim.loader.enable()
 
-vim.o.signcolumn = "yes"
--- vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
 vim.o.autoindent = true
-vim.o.smartindent = true
 vim.o.colorcolumn = "121"
--- vim.o.updatetime = 100
-vim.o.scrolloff = 10
 vim.o.cursorline = true
 vim.o.cursorlineopt = "number"
-vim.o.jumpoptions = "stack,view"
-vim.o.incsearch = true
+vim.o.expandtab = true
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
-vim.o.wrap = false
-vim.o.winborder = "rounded"
-vim.o.termguicolors = true
-vim.o.splitright = true
+vim.o.incsearch = true
+vim.o.jumpoptions = "stack,view"
 vim.o.mouse = ""
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.scrolloff = 10
+vim.o.shiftwidth = 4
+vim.o.signcolumn = "yes"
+vim.o.smartindent = true
+vim.o.softtabstop = 4
+vim.o.splitright = true
+vim.o.tabstop = 4
+vim.o.termguicolors = true
+vim.o.winborder = "rounded"
+vim.o.wrap = false
 vim.opt.fillchars:append(",eob: ")
 
 vim.diagnostic.config({
-    virtual_text = false,
-    virtual_lines = true,
+    virtual_text = true,
+    virtual_lines = false,
     underline = true,
-    --update_in_insert = false,
+    update_in_insert = false,
     severity_sort = true,
     float = {
         source = true
