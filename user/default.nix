d58f6fe1ad903663496bdf13bc86dae21158f043
vBrunhild <brunomoretti100@gmail.com>
2025-08-27 04:02:54 +0000

    fish automatically opens zellij

--- user/default.nix
+++ user/default.nix
@@ -1,45 +1,53 @@
 rec {
-  packages = pkgs: let
-    inherit (pkgs) callPackage;
-  in {
-    fish = pkgs.fish;
-    neovim = callPackage ./wrapped/neovim {};
-    zellijPlugins = callPackage ./wrapped/zellij-plugins.nix {};
-  };
-
-  module = {pkgs, ...}: {
-    config = {
-      environment = {
-        systemPackages = builtins.attrValues (packages pkgs);
-        variables = {
-          RIPGREP_CONFIG_PATH = "$HOME/.config/ripgrep/.ripgreprc";
-          EDITOR = "nvim";
-          VISUAL = "nvim";
-        };
-        pathsToLink = ["/share/zellij-plugins"];
-      };
-
-      programs.fish = {
-        enable = true;
-        interactiveShellInit = ''
-          set fish_greeting
-        '';
-      };
-
-      programs.bat.enable = true;
-      programs.direnv.enable = true;
-      programs.ssh.startAgent = true;
-      programs.starship.enable = true;
-      programs.zoxide.enable = true;
-
-      # stop fish from increasing build times by an ridiculous amount
-      documentation.man.generateCaches = false;
+  packages =
+    pkgs:
+    let
+      inherit (pkgs) callPackage;
+    in
+    {
+      fish = pkgs.fish;
+      neovim = callPackage ./wrapped/neovim { };
+      zellijPlugins = callPackage ./wrapped/zellij-plugins.nix { };
     };
 
-    imports = [
-      ./packages.nix
-      ./git.nix
-      ./homix-config.nix
-    ];
-  };
+  module =
+    { pkgs, ... }:
+    {
+      config = {
+        environment = {
+          systemPackages = builtins.attrValues (packages pkgs);
+          variables = {
+            RIPGREP_CONFIG_PATH = "$HOME/.config/ripgrep/.ripgreprc";
+            EDITOR = "nvim";
+            VISUAL = "nvim";
+          };
+          pathsToLink = [ "/share/zellij-plugins" ];
+        };
+
+        programs.fish = {
+          enable = true;
+          interactiveShellInit =
+            # fish
+            ''
+              set fish_greeting
+              eval (zellij setup --generate-auto-start fish | string collect)
+            '';
+        };
+
+        programs.bat.enable = true;
+        programs.direnv.enable = true;
+        programs.ssh.startAgent = true;
+        programs.starship.enable = true;
+        programs.zoxide.enable = true;
+
+        # stop fish from increasing build times by an ridiculous amount
+        documentation.man.generateCaches = false;
+      };
+
+      imports = [
+        ./packages.nix
+        ./git.nix
+        ./homix-config.nix
+      ];
+    };
 }
