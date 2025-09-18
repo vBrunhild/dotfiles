7eb95528ec842bfccdf828d7fcfd14593a95c97e
vBrunhild <brunomoretti100@gmail.com>
2025-09-18 11:40:53 -0400

    update some python configs

--- devflakes/python/flake.nix
+++ devflakes/python/flake.nix
@@ -5,33 +5,28 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs =
-    { nixpkgs, ... }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-    in
-    {
-      environment.localBinInPath = true;
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  in {
+    environment.localBinInPath = true;
 
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.basedpyright
-              pkgs.ruff
-              pkgs.uv
-            ];
-          };
-        }
-      );
-    };
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.pyrefly
+            pkgs.ruff
+            pkgs.uv
+          ];
+        };
+      }
+    );
+  };
 }
