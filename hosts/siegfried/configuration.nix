d66c392cd5579cb15fbd3fbb909f47def59ecad4
vBrunhild <brunomoretti100@gmail.com>
2026-02-23 08:16:14 -0400

    feat: enable bluetooth

--- hosts/siegfried/configuration.nix
+++ hosts/siegfried/configuration.nix
@@ -7,8 +7,8 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
   networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
