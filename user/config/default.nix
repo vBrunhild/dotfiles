53c43ffad3cb21d4cee23f42b64899cc24526119
vBrunhild <brunomoretti100@gmail.com>
2026-02-23 08:16:14 -0400

    feat: add niri config file

--- user/config/default.nix
+++ user/config/default.nix
@@ -3,6 +3,7 @@
     ".config/bat/config".source = ./bat/config.rc;
     ".config/bottom/bottom.toml".source = ./bottom/config.toml;
     ".config/jj/config.toml".source = ./jj/config.toml;
+    ".config/niri/config.kdl".source = ./niri/config.kdl;
     ".config/wezterm/wezterm.lua".source = ./wezterm/config.lua;
     ".config/zellij/config.kdl".source = ./zellij/config.kdl;
     ".config/zellij/layouts/default.kdl".source = ./zellij/layouts/default.kdl;
