27e8ce9626701b0095824be84eb647b182108956
vBrunhild <brunomoretti100@gmail.com>
2026-02-23 08:16:14 -0400

    feat: add spotify player and remove wezterm configs

--- user/config/default.nix
+++ user/config/default.nix
@@ -2,9 +2,10 @@
   homix = {
     ".config/bat/config".source = ./bat/config.rc;
     ".config/bottom/bottom.toml".source = ./bottom/config.toml;
+    ".config/flameshot/flameshot.ini".source = ./flameshot/flameshot.ini;
     ".config/jj/config.toml".source = ./jj/config.toml;
     ".config/niri/config.kdl".source = ./niri/config.kdl;
-    ".config/wezterm/wezterm.lua".source = ./wezterm/config.lua;
+    ".config/spotify-player/app.toml".source = ./spotify_player/app.toml;
     ".config/zellij/config.kdl".source = ./zellij/config.kdl;
     ".config/zellij/layouts/default.kdl".source = ./zellij/layouts/default.kdl;
     ".ripgreprc".source = ./rigprep/config.rc;
