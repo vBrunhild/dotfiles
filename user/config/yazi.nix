005e5a72802abc2f3a2aefda6deb9eef0752d8ad
vBrunhild <brunomoretti100@gmail.com>
2026-03-04 15:51:20 -0400

    feat: add theme to yazi and make it work as a file picker

--- user/config/yazi.nix
+++ user/config/yazi.nix
@@ -12,8 +12,67 @@
     };
   };
 
-  xdg.configFile."xdg-desktop-portal-termfilechooser/config".text = ''
-    [filechooser]
-    cmd=foot --title=filepicker ${pkgs.yazi}/bin/yazi
-  '';
+  xdg.configFile."xdg-desktop-portal-termfilechooser/config".text = let
+    yazi-wrapper = pkgs.writeShellScript "yazi-filepicker" ''
+      #!/usr/bin/env sh
+
+      multiple="$1"
+      directory="$2"
+      save="$3"
+      path="$4"
+      out="$5"
+      debug="$6"
+
+      set -e
+
+      if [ "$debug" = 1 ]; then
+          set -x
+      fi
+
+      cmd="${pkgs.yazi}/bin/yazi"
+      termcmd="''${TERMCMD}"
+
+      if [ "$save" = "1" ]; then
+          # save a file
+          set -- --chooser-file="$out" "$path"
+      elif [ "$directory" = "1" ]; then
+          # upload files from a directory
+          set -- --chooser-file="$out" --cwd-file="$out"".1" "$path"
+      elif [ "$multiple" = "1" ]; then
+          # upload multiple files
+          set -- --chooser-file="$out" "$path"
+      else
+          # upload only 1 file
+          set -- --chooser-file="$out" "$path"
+      fi
+
+      command="$termcmd $cmd"
+      for arg in "$@"; do
+          # escape double quotes
+          escaped=$(printf "%s" "$arg" | sed 's/"/\\"/g')
+          # escape special
+          command="$command \"$escaped\""
+      done
+
+      sh -c "$command"
+
+      if [ "$directory" = "1" ]; then
+          if [ ! -s "$out" ] && [ -s "$out"".1" ]; then
+              cat "$out"".1" > "$out"
+              rm "$out"".1"
+          else
+              rm "$out"".1"
+          fi
+      fi
+    '';
+  in
+    # toml
+    ''
+      [filechooser]
+      cmd=${yazi-wrapper}
+      default_dir=$HOME
+      env=TERMCMD=foot -T=filepicker
+      open_mode=suggested
+      save_mode=last
+    '';
 }
