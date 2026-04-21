01a6fc1ed5ba972d89141107050a65c8e33c0ff9
vBrunhild <brunomoretti100@gmail.com>
2026-04-20 20:23:50 -0400

    feat: add phpantom lsp

--- devflakes/php/flake.nix
+++ devflakes/php/flake.nix
@@ -3,9 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {nixpkgs, ...}: let
+  outputs = inputs @ {nixpkgs, ...}: let
     forAllSystems = nixpkgs.lib.genAttrs [
       "aarch64-darwin"
       "aarch64-linux"
@@ -13,21 +18,39 @@
       "x86_64-linux"
     ];
 
-    pkgsFor = system:
-      import nixpkgs {inherit system;};
-
-    allPkgs = forAllSystems pkgsFor;
+    allPkgs = forAllSystems (
+      system:
+        import nixpkgs {
+          inherit system;
+          overlays = [inputs.rust-overlay.overlays.default];
+        }
+    );
   in {
     devShells = forAllSystems (
       system: let
         pkgs = allPkgs.${system};
+        toolchain = pkgs.rust-bin.stable.latest.default;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        phpantom-lsp = rustPlatform.buildRustPackage {
+          pname = "phpantom_lsp";
+          version = "0.7.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "AJenbo";
+            repo = "phpantom_lsp";
+            rev = "0.7.0";
+            hash = "sha256-ZmtOdoxXkwn2IDg7RyQ9KG0RNz5mrGDMcESfcOSR3Ig=";
+          };
+          cargoHash = "sha256-pXP4qItYgmUXVx9XwMdS6WLVc5lP7P4VX9+0TbhYrUc=";
+          doCheck = false;
+        };
       in {
         default = pkgs.mkShell {
           buildInputs = [
-            pkgs.laravel
-            pkgs.php
-            pkgs.php84Packages.psalm
-            pkgs.phpactor
+            phpantom-lsp
           ];
         };
       }
