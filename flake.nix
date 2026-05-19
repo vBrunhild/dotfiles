a40c03bf0c73cd55c5e6c84dbbb3f144d1dfd968
vBrunhild <brunomoretti100@gmail.com>
2026-05-19 11:22:58 -0400

    feat: update nix and add caches

--- flake.nix
+++ flake.nix
@@ -40,6 +40,17 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://noctalia.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
+  };
+
   outputs = inputs @ {nixpkgs, ...}: let
     user = import ./user;
 
